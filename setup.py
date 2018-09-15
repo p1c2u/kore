d1485fea9c8bde8c85af11e10e1681c09f4124fb
p1c2u <maciag.artur@gmail.com>
2018-09-15 18:37:11 +0100

    python 2.6 support dropped

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries',
     ],
